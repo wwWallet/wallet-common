c1fa584cc7b0de0e00d43240f972ae96ac7d2792
kkmanos <koukoularisman@gmail.com>
2025-05-19 10:06:48 +0300

    schema updates

--- src/schemas/OpenidAuthorizationServerMetadataSchema.ts
+++ src/schemas/OpenidAuthorizationServerMetadataSchema.ts
@@ -8,9 +8,13 @@ export const OpenidAuthorizationServerMetadataSchema = z.object({
 	authorization_challenge_endpoint: z.string().optional(),
 	require_pushed_authorization_requests: z.boolean().optional(),
 	token_endpoint_auth_methods_supported: z.array(z.string()).optional(),
+	token_endpoint_auth_signing_alg_values_supported: z.array(z.string()).optional(),
 	response_types_supported: z.array(z.string()).optional(),
 	code_challenge_methods_supported: z.array(z.string()).optional(),
 	dpop_signing_alg_values_supported: z.array(z.string()).optional(),
+	scopes_supported: z.array(z.string()).optional(),
+	grant_types_supported: z.array(z.string()).optional(),
+	jwks_uri: z.string(),
 });
 
 export type OpenidAuthorizationServerMetadata = z.infer<typeof OpenidAuthorizationServerMetadataSchema>;
