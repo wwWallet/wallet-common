wwWallet/wallet-frontend

Implement JWP with Split-BBS

src/bbs/index.ts
@@ -1099,7 +1099,7 @@ function createSuite(suite: SuiteParams): CipherSuite {
 	};
 }
 
-type PointG1 = ProjPointType<bigint>;
+export type PointG1 = ProjPointType<bigint>;
 type PointG2 = ProjPointType<Fp2>;
 type HashToScalarFunc = (msg_octets: BufferSource, dst: BufferSource) => Promise<bigint>;
 type MessagesToScalarsFunc = (messages: BufferSource[], api_id: BufferSource) => Promise<bigint[]>;

wwWallet/wallet-common

src/bbs/index.ts
@@ -1094,7 +1094,7 @@ function createSuite(suite: SuiteParams): CipherSuite {
 	};
 }
 
-type PointG1 = WeierstrassPoint<bigint>;
+export type PointG1 = WeierstrassPoint<bigint>;
 type PointG2 = WeierstrassPoint<Fp2>;
 type HashToScalarFunc = (msg_octets: BufferSource, dst: BufferSource) => Promise<bigint>;
 type MessagesToScalarsFunc = (messages: BufferSource[], api_id: BufferSource) => Promise<bigint[]>;