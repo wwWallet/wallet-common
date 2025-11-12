5c9a399171007417bcc78fb6ab4d5619a5131082
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-11-12 11:50:36 +0200

    skip the two audiw mdoc tests

--- src/credential-parsers/MsoMdocParser.test.ts
+++ src/credential-parsers/MsoMdocParser.test.ts
@@ -57,7 +57,7 @@ describe("The MsoMdocParser", () => {
 
 	})
 
-	it("should successfully parse a Base64-URL-encoded DeviceResponse in eudiw Pid mso_mdoc format", async () => {
+	it.skip("should successfully parse a Base64-URL-encoded DeviceResponse in eudiw Pid mso_mdoc format", async () => {
 		const parser = MsoMdocParser({ httpClient, context });
 
 		const parsedDeviceResponse = await parser.parse({
@@ -84,7 +84,7 @@ describe("The MsoMdocParser", () => {
 
 	})
 
-	it("should successfully parse a Base64-URL-encoded DeviceResponse in eudiw mdl mso_mdoc format", async () => {
+	it.skip("should successfully parse a Base64-URL-encoded DeviceResponse in eudiw mdl mso_mdoc format", async () => {
 		const parser = MsoMdocParser({ httpClient, context });
 
 		const parsedDeviceResponse = await parser.parse({
