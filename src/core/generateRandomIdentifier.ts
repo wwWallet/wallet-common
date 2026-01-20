da3059df72df5545c1d54f9e82fae029f534f083
kkmanos <koukoularisman@gmail.com>
2026-01-20 13:46:16 +0200

    fix: use crypto global in core/generateRandomIdentifier function;

--- src/core/generateRandomIdentifier.ts
+++ src/core/generateRandomIdentifier.ts
@@ -1,7 +1,6 @@
-import * as webcrypto from "uncrypto";
 
 export function generateRandomIdentifier(length: number) {
 	const array = new Uint8Array(length);
-	webcrypto.getRandomValues(array);
+	crypto.getRandomValues(array);
 	return Array.from(array, byte => byte.toString(16).padStart(2, '0')).join('');
 }
