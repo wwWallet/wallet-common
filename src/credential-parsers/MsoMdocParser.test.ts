6d3fb8d451f9038008e55863ac5e32f1938d29af
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-02-12 16:09:30 +0200

    remove openidvci 1.0 comments

--- src/credential-parsers/MsoMdocParser.test.ts
+++ src/credential-parsers/MsoMdocParser.test.ts
@@ -57,7 +57,6 @@ describe("The MsoMdocParser", () => {
 
 	})
 
-	// Needs update openid4vci to 1.0
 	it("should successfully parse a Base64-URL-encoded DeviceResponse in eudiw Pid mso_mdoc format", async () => {
 		const parser = MsoMdocParser({ httpClient, context });
 
@@ -85,7 +84,6 @@ describe("The MsoMdocParser", () => {
 
 	})
 
-	// Needs update openid4vci to 1.0
 	it("should successfully parse a Base64-URL-encoded DeviceResponse in eudiw mdl mso_mdoc format", async () => {
 		const parser = MsoMdocParser({ httpClient, context });
 
