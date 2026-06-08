24cd988e2324536f3542648aa01c52a4c1bf06f3
Vasileios Markopoulos <vmarkop@gunet.gr>
2026-06-08 10:38:18 +0300

    Support MDOC birth_date object with approximate mask

--- src/utils/cborDate.test.ts
+++ src/utils/cborDate.test.ts
@@ -1,5 +1,5 @@
 import { assert, describe, it } from "vitest";
-import { formatCborDate, isCborDate } from "./cborDate";
+import { formatCborBirthDate, formatCborDate, isCborBirthDate, isCborDate } from "./cborDate";
 
 describe("isCborDate", () => {
 
@@ -36,7 +36,7 @@ describe("isCborDate", () => {
 	});
 });
 
-describe("isCborDate", () => {
+describe("formatCborDate", () => {
 
 	it("can format a CBOR Date object", () => {
 		const cborDate = {
@@ -59,6 +59,74 @@ describe("isCborDate", () => {
 		console.log(formattedDate);
 
 		assert(JSON.stringify(invalidCborDateObject) === JSON.stringify(formattedDate));
+	});
+
+});
+
+describe("isCborBirthDate", () => {
+
+	it("can match a CBOR Birth Date object", () => {
+		const cborBirthDate = {
+			birth_date: {
+				date: '01-01-2026'
+			}
+		};
+
+		assert(isCborBirthDate(cborBirthDate));
+	});
+
+	it("can match a CBOR Birth Date object including approximate_mask", () => {
+		const cborBirthDate = {
+			birth_date: {
+				date: '01-01-2026'
+			},
+			approximate_mask: 'XXXXXXXX'
+		};
+
+		assert(isCborBirthDate(cborBirthDate));
+	});
+
+	it("does not match an object that is not a CBOR birth date", () => {
+		const randomObject = {
+			date: 'bar'
+		};
+
+		assert(!isCborBirthDate(randomObject));
+	});
+
+
+	it("does not match a simple date string", () => {
+		const dateString = "04-06-2026";
+
+		assert(!isCborBirthDate(dateString));
+	});
+});
+
+describe("formatCborBirthDate", () => {
+
+	it("can format a CBOR Birth Date object", () => {
+		const cborBirthDate = {
+			birth_date: {
+				date: '01-01-2026'
+			}
+		};
+
+		const formattedDate = formatCborBirthDate(cborBirthDate);
+		console.log(formattedDate);
+		assert(formattedDate === '01/01/2026');
+	});
+
+	it("does not alter an invalid CBOR object", () => {
+		const invalidCborBirthDateObject = {
+			invalid_date: '01-01-2026',
+		};
+
+		const formattedDate = formatCborBirthDate(invalidCborBirthDateObject as any);
+
+		console.log(invalidCborBirthDateObject);
+		console.log(formattedDate);
+
+		assert(JSON.stringify(invalidCborBirthDateObject) === JSON.stringify(formattedDate));
 	});
 
 });
